0cdf591cda5b4a9a810625dfcf223c284ca7ed8b
Ivanito Hjelmeland <ivan.hjelmeland@gmail.com>
2012-05-23 17:01:38 +0200

    Sprkendring - Norsk - Engelsk

--- wp-content/plugins/Apartmentviewer/admin.php
+++ wp-content/plugins/Apartmentviewer/admin.php
@@ -2,8 +2,8 @@
 
 function create_theme_options_page() {
   add_menu_page('ApartmentViewer Settings', 'ApartmentViewer', 'administrator', __FILE__, 'build_options_page', plugins_url('/images/icon.png', __FILE__));
-  add_submenu_page(__FILE__, 'general', 'Settings', 'administrator', __FILE__, '', 'general_funksjon');
-  add_submenu_page( __FILE__, 'About', 'About', 'administrator', 'my-custom-submenu-page', 'my_custom_submenu_page_callback' ); 
+  add_submenu_page(__FILE__, 'general', 'Instillinger', 'administrator', __FILE__, '', 'general_funksjon');
+  add_submenu_page( __FILE__, 'About', 'Om', 'administrator', 'my-custom-submenu-page', 'my_custom_submenu_page_callback' ); 
 }
 add_action('admin_menu', 'create_theme_options_page');
 
@@ -31,8 +31,8 @@ function build_options_page() {
     	<style>
 
 		div#myBackground {
-		background-image: url("http://www.tunafishmedia.com/prosjekt/webutv/wp-content/plugins/CinemaTuna/images/cinemapop.jpg");
-		width: 1000px; height: 1000px; 
+		
+		width: 1000px; 
 		background-repeat:no-repeat;}
 
 
@@ -79,7 +79,7 @@ add_action('admin_init', 'register_and_build_fields');
 function register_and_build_fields() {
   register_setting('plugin_options', 'plugin_options', 'validate_setting');
   add_settings_section('main_section', 'Widget-innstilling', 'section_cb', __FILE__);
-  add_settings_section('about_section', 'About', 'section_cb', __FILE__);
+  add_settings_section('about_section', 'Om ApartmentViewer', 'section_cb', __FILE__);
   add_settings_field('tit_color', 'Tittelfarge:', 'title_color', __FILE__, 'main_section' );
   add_settings_field('tit_font', 'Tittelfont:', 'title_font', __FILE__, 'main_section');
   add_settings_field('tit_size', 'Tittelst&oslash;rrelse:', 'title_size', __FILE__, 'main_section');
@@ -106,38 +106,67 @@ function author_innstilling() {
 // Funskjon for å endre fargen på widget-titler.
 function title_color() {
   $options = get_option('plugin_options');
+  if($options['tit_color'] == ''){
+  echo "<input name='plugin_options[tit_color]' type='text' value='#808080' />";
+  }
+  else{
   echo "<input name='plugin_options[tit_color]' type='text' value='{$options['tit_color']}' />";
+  }
 }
 
 // Funskjon for å endre fonten på widhget-titler.
 function title_font() {
   $options = get_option('plugin_options');
+  if($options['tit_font'] == ''){
+  echo "<input name='plugin_options[tit_font]' type='text' value='Verdana' />";
+  }
+  else{
   echo "<input name='plugin_options[tit_font]' type='text' value='{$options['tit_font']}' />";
+  }
 }
 
 // Funskjon for å endre størrelsen på widget-titler.
 function title_size() {
   $options = get_option('plugin_options');
+  if($options['tit_size'] == ''){
+  echo "<input name='plugin_options[tit_size]' type='text' value='14px' />";
+  }
+  else{
   echo "<input name='plugin_options[tit_size]' type='text' value='{$options['tit_size']}' />";
-  
+  }
 }
 
 // Funskjon for å endre fargen på widget-innhold.
 function tekst_farge() {
   $options = get_option('plugin_options');
+    if($options['tek_color'] == ''){
+  echo "<input name='plugin_options[tek_color]' type='text' value='#808080' />";
+  }
+  else{
   echo "<input name='plugin_options[tek_color]' type='text' value='{$options['tek_color']}' />";
+  }
 }
 
 // Funksjon for å endre fonten på widget-innhold.
 function tekst_font() {
   $options = get_option('plugin_options');
+   if($options['tek_font'] == ''){
+  echo "<input name='plugin_options[tek_font]' type='text' value='Verdana' />";
+  }
+  else{
   echo "<input name='plugin_options[tek_font]' type='text' value='{$options['tek_font']}' />";
+  }
 }
 
 // Funksjon for å endre bredden på meglerbildet.
 function picture_width() {
   $options = get_option('plugin_options');
+    if($options['pic_width'] == ''){
+  echo "<input name='plugin_options[pic_width]' type='text' value='200px' />";
+  }
+  else{
   echo "<input name='plugin_options[pic_width]' type='text' value='{$options['pic_width']}' />";
+}
 }
 
 function about_section() {
